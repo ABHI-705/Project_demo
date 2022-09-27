42b79084b869e43d62de9b316c9a29df5f51a700
ABHI-705 <111434086+ABHI-705@users.noreply.github.com>
2022-09-27 11:55:48 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+st.title("Abhinav Kumar Tripathi")
+st.write("MSC DSAI")
+st.header("Python")
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
+st.caption('This is a Geometric Series')
+import pandas as pd
+import numpy as np
+import altair as alt
+df = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=['a', 'b', 'c'])
+
+c = alt.Chart(df).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+st.write(c)
+st.line_chart(df)
+st.area_chart(df)
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
